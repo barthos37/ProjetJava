40bac8fdc6de2e28adb0fd487b560f42ddf8f1f3
barthos37 <101190882+barthos37@users.noreply.github.com>
2022-04-09 20:51:06 +0200

    Add files via upload

---
+++ main.java
@@ -0,0 +1,14 @@
+import modele.*;
+import vue.*;
+import controlleur.*;
+
+import java.util.ArrayList;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		AccueuilControlleur ctrl = new AccueuilControlleur();
+	}
+}
